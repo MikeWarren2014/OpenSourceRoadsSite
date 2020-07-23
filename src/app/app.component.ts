06ba1ec60a42de945589cdcfdc0b14e3f367d382
MikeWarren2014 <mwarren04011990@gmail.com>
2020-07-23 17:09:26 -0400

    Making the top header responsive,fixing the nav side bar responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
-import { Component,  ViewChild } from '@angular/core';
-import { BannerComponent } from './components/widgets/page/banner/banner.component';
+import { Component, ViewChild } from "@angular/core";
+import { BannerComponent } from "./components/widgets/page/banner/banner.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css", "./app.component.responsive.css"],
 })
-export class AppComponent  {
-  title = 'OpenSourceRoads';
-   @ViewChild(BannerComponent) banner : BannerComponent;
+export class AppComponent {
+  title = "OpenSourceRoads";
+  @ViewChild(BannerComponent) banner: BannerComponent;
 
-   constructor() {
-  }
-
-
+  constructor() {}
 }
