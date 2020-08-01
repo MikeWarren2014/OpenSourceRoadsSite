6f5bdfa4b088db4daa3b02d2a19626e6ba5887a8
MikeWarren2014 <mwarren04011990@gmail.com>
2020-08-01 04:10:08 -0400

    Took out the hardcoded buttton

--- src/app/components/sections/donate/donate-section.component.ts
+++ src/app/components/sections/donate/donate-section.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { BasePageSectionComponent } from "../base-page-section/base-page-section.component";
 
 @Component({
-  selector: "app-donate",
+  selector: "app-donate-section",
   templateUrl: "./donate-section.component.html",
   styleUrls: ["./donate-section.component.css"],
 })
